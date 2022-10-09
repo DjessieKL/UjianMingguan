1bbdb8e1ba4ba46571fa0dd83d29f4cd3e8e93b0
Jessy Dimpudus <jkldimpudus@gmail.com>
2022-10-10 00:48:09 +0800

    FirstCommit

---
+++ src/main/java/JuaraCoding/MingguKeDua/NumberOne.java
@@ -0,0 +1,18 @@
+package JuaraCoding.MingguKeDua;
+
+public class NumberOne {
+    public static void main(String[] args) {
+        int a = 1;
+        for (int i = 9; i >= 0; i--) {
+            for (int j = a; j <= i; j++) {
+                if (j % 2 == 1) {
+                    System.out.print(j);
+                } else {
+                    System.out.print("_");
+                }
+            }
+            a++;
+            System.out.println();
+        }
+    }
+}
